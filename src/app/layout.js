0abc0c51f84bd3cf591016dc6023558c132c0169
JuanseHevia <jh216@rice.edu>
2024-10-25 23:53:09 -0500

    Header component. add Logo to header. changed alias.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter, Manrope } from 'next/font/google'
-import {cx} from '@/utils/index'
+import {cx} from '@/src/utils/index'
+import Header from '@/src/components/Header'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' , variable: '--font-in'})
 const manrope = Manrope({ subsets: ['latin'], display: 'swap', variable: '--font-mr'})
@@ -13,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={cx(inter.variable, manrope.variable, "font-mr bg-light dark:bg-dark")}>{children}</body>
+      <body className={cx(inter.variable, manrope.variable, "font-mr bg-ligh")}>
+        <Header />
+        {children}
+        </body>
     </html>
   )
 }
