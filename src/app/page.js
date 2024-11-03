99961cd4ac8b81b3e61c75f7171f80133e8328f0
JuanseHevia <jh216@rice.edu>
2024-11-03 17:12:09 -0600

    added featured posts section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HomeCoverSection from "@/src/components/Home/HomeCoverSection";
 import { allBlogs } from "@/.contentlayer/generated";
+import FeaturedPosts from "../components/Home/FeaturedPosts";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs} />
     </main>
   )
 }
--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 import { sortBlogs } from '@/src/utils';
 import Link from 'next/link';
+import { Tag } from '../Elements/Tag';
 
 const HomeCoverSection = ({ blogs }) => {
     const sortedBlogs = sortBlogs(blogs);
-    const lastBlog = sortedBlogs[0];
+    const lastBlog = sortedBlogs[1];
     return (
         <div className='w-full inline-block'>
             <article className='flex flex-col items-start justify-end mx-10 relative h-[85vh]'>
                 <div className='absolute top-0 left-0 bottom-0 right-0 h-full 
-                bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0'/>
+                bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0'/>
                 <Image src={lastBlog.image.filePath.replace("../public", "")}
                     alt={lastBlog.title}
                     placeholder="blur"
@@ -19,11 +20,19 @@ const HomeCoverSection = ({ blogs }) => {
                     className="w-full h-full object-center object-cover rounded-3xl -z-10"
                 />
                 <div className='w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light'>
-                    <Link href={`categories/${lastBlog.tags[0]}}`}>
-                        {lastBlog.tags[0]}
+                    <Tag link={`/categories/${lastBlog.tags[0]}`} name={lastBlog.tags[0]} />
+                    <Link href={`/blogs/${lastBlog.url}`}>
+                        <h1 className='text-5xl font-bold'>
+                            <span className='bg-gradient-to-r
+                                             from-accent to-accent bg-[length:0px_6px]
+                                             hover:bg-[length:100%_6px] bg-left-bottom 
+                                             bg-no-repeat transition-[background-size] duration-500'>
+                                {lastBlog.title}
+                            </span>
+
+                        </h1>
                     </Link>
-                    <h1 className='text-5xl font-bold'>{lastBlog.title}</h1>
-                    <p>{lastBlog.description}</p>
+                    <p className='inline-block mt-4 text-xl font-in'>{lastBlog.description}</p>
                 </div>
             </article>
         </div>
